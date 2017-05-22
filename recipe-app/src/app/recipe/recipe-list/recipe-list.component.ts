00a997466e9f9992813bd72577c78aac282e8eb4
ChristophSchmidl <christoph.schmidl@gmx.net>
2017-05-22 18:53:23 +0000

    Added polling strategy

---
+++ recipe-app/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styles: []
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  recipes = new Recipe[]("Dummy", "Dummy", "http://guides.global/images/guides/global/dummy_web_page.jpg");
+  selectedRecipe: Recipe;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+
+}
